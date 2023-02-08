2f04187bd6bfd14c49d08b614f471143535752d5
dianexjeong <zuphyr14@gmail.com>
2023-02-08 21:52:29 +0900

    feat: add text elements in application page

--- src/pages/Application.js
+++ src/pages/Application.js
@@ -1,11 +1,20 @@
 import logo from "../assets/logoFull.svg";
+import lion from "../assets/lionWink.svg";
 import styled from "styled-components";
+import { Space } from "../components/atoms";
 
 const Application = () => {
   return (
     <>
       <Flex>
-        <LogoContainer src={logo} />
+        <ImgContainer src={logo} />
+        <FlexRow>
+          <ImgContainer src={lion} />
+          <Space margin="0px 10px 0px 0px" />
+          <DDate>D-7</DDate>
+          <Space margin="0px 20px 0px 0px" />
+          <DTime>17:30:40</DTime>
+        </FlexRow>
       </Flex>
     </>
   );
@@ -13,14 +22,27 @@ const Application = () => {
 
 export default Application;
 
-const LogoContainer = styled.img`
-  width: 279px;
-  height: 48px;
-`;
+const ImgContainer = styled.img``;
 
 const Flex = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+const FlexRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const DDate = styled.div`
+  font-family: Wakeup;
+  font-size: 55px;
+`;
+
+const DTime = styled.div`
+  font-family: Wakeup;
+  font-size: 36px;
 `;
