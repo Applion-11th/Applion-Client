5fe7ae30dd405e2091f50bcbb5d499fe9304fe32
dianexjeong <zuphyr14@gmail.com>
2023-02-08 21:09:17 +0900

    feat: add responsivity in web

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 import { Header } from "./components";
+import styled from "styled-components";
 
 // pages
 import Main from "./pages/Main";
@@ -9,19 +11,33 @@ import Register from "./pages/Register";
 import Info from "./pages/Info";
 
 const App = () => {
+  const isPC = useMediaQuery({
+    query: "(min-width: 768px) and (max-width: 1920px)",
+  });
+
   return (
     <>
-      <BrowserRouter>
-        {window.location.pathname !== "/" ? <Header /> : <></>}
-        <Routes>
-          <Route path="/" exact element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/info" element={<Info />} />
-        </Routes>
-      </BrowserRouter>
+      {isPC ? (
+        <>
+          <BrowserRouter>
+            {window.location.pathname !== "/" ? <Header /> : <></>}
+            <Routes>
+              <Route path="/" exact element={<Main />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/info" element={<Info />} />
+            </Routes>
+          </BrowserRouter>
+        </>
+      ) : (
+        <Text>PC환경에서 접속해주세요!</Text>
+      )}
     </>
   );
 };
 
 export default App;
+
+const Text = styled.div`
+  font-size: 50px;
+`;
