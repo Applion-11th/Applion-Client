28035e29e7899593239f1d48e9bd002ebe8626cd
dianexjeong <zuphyr14@gmail.com>
2023-02-12 09:14:29 +0900

    feat: add main page heading elements

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,32 @@
+import { Button } from "../components/atoms";
+import styled from "styled-components";
+import logoFull from "../assets/logoFull.svg";
+
 const Main = () => {
   return (
     <>
-      <p>메인 페이지입니다.</p>
+      <Button text="지금 바로 지원하기" fontSize="18px" width="210px" height="59px" borderRadius="20px" />
+      <Flex>
+        <LogoContainer src={logoFull} />
+      </Flex>
     </>
   );
 };
 
 export default Main;
+
+const LogoContainer = styled.img`
+  width: 767px;
+  height: 132px;
+`;
+
+const Flex = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonContainer = styled.div`
+  position: absolute;
+`;
