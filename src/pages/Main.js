73ee04e6f9405646be2a293813559a59f483f1be
dianexjeong <zuphyr14@gmail.com>
2023-02-12 11:41:21 +0900

    feat: add scroll animation to main page

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,60 +1,97 @@
 import { Button, Space } from "../components/atoms";
+import { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import logoFull from "../assets/logoFull.svg";
 import logoFooter from "../assets/logoFooter.svg";
 import palette from "../styles/colors";
 
 const Main = () => {
+  const FadeInSection = (props) => {
+    const [isVisible, setVisible] = useState(false);
+    const domRef = useRef();
+    useEffect(() => {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => setVisible(entry.isIntersecting));
+      });
+      observer.observe(domRef.current);
+      return () => observer.unobserve(domRef.current);
+    }, []);
+
+    return (
+      <FadeContainer ref={domRef} isVisible={isVisible ? true : false}>
+        {props.children}
+      </FadeContainer>
+    );
+  };
+
   return (
     <>
       <ButtonContainer>
         <Button text="지금 바로 지원하기" fontSize="18px" width="210px" height="59px" borderRadius="20px" />
       </ButtonContainer>
       <Section1>
-        <Space height="114px" />
-        <LogoContainer src={logoFull} />
-        <TextIntro>Welcome to Sogang Likelion!</TextIntro>
-        <TextIntroContainer>
-          <TextIntro>Let's begin</TextIntro>
-          <Space margin="0px 10px 0px 0px" />
-          <TextIntroRed>the adventure</TextIntroRed>
-        </TextIntroContainer>
+        <FadeInSection>
+          <CenterContainer>
+            <Space height="114px" />
+            <LogoContainer src={logoFull} />
+            <TextIntro>Welcome to Sogang Likelion!</TextIntro>
+            <TextIntroContainer>
+              <TextIntro>Let's begin</TextIntro>
+              <Space margin="0px 10px 0px 0px" />
+              <TextIntroRed>the adventure</TextIntroRed>
+            </TextIntroContainer>
+          </CenterContainer>
+        </FadeInSection>
       </Section1>
       <Section2>
-        <TextContainer>
-          <Space height="200px" />
-          <TextBold>Possibility to Reality</TextBold>
-          <Space height="25px" />
-          <Text>
-            가능성을 현실로 만들어가는 멋쟁이사자처럼은
-            <br />
-            꿈꾸는 자들을 위해 디지털 전문 교육을 제공하여
-            <br />
-            IT분야의 TECH 리더를 양성합니다.
-          </Text>
-        </TextContainer>
+        <FadeInSection>
+          <RowContainer>
+            <TextContainer>
+              <TextBold>Possibility to Reality</TextBold>
+              <Space height="25px" />
+              <Text>
+                가능성을 현실로 만들어가는 멋쟁이사자처럼은
+                <br />
+                꿈꾸는 자들을 위해 디지털 전문 교육을 제공하여
+                <br />
+                IT분야의 TECH 리더를 양성합니다.
+              </Text>
+            </TextContainer>
+            <Space margin="200px 50px 0px 0px" />
+          </RowContainer>
+        </FadeInSection>
       </Section2>
       <Section3>
-        <Space margin="0px 60px 0px 0px" />
-        <TimeTable>
-          <TextBold>지원 일정</TextBold>
-          <TextList>서류 지원 : 2023.02.20 ~ 2023.03.09</TextList>
-          <TextList>서류 발표 : 2023.03.13</TextList>
-          <TextList>면접 : 2023.03.15 ~ 2023.03.17</TextList>
-          <TextList>최종 발표 : 2023.03.20</TextList>
-        </TimeTable>
+        <FadeInSection>
+          <RowContainer>
+            <Space margin="0px 60px 0px 0px" />
+            <TimeTable>
+              <TextBold>지원 일정</TextBold>
+              <TextList>서류 지원 : 2023.02.20 ~ 2023.03.09</TextList>
+              <TextList>서류 발표 : 2023.03.13</TextList>
+              <TextList>면접 : 2023.03.15 ~ 2023.03.17</TextList>
+              <TextList>최종 발표 : 2023.03.20</TextList>
+            </TimeTable>
+          </RowContainer>
+        </FadeInSection>
       </Section3>
       <Section4>
-        <TextBold>웹 개발 기본 과정을 합께 배웁니다</TextBold>
-        <Text>멋쟁이 사자처럼 11기 아기사자들은 TECHIT 사이트에서 진행되는 개발 강의를 듣게 됩니다.</Text>
-        <Button text="TECHIT 둘러보기" fontSize="18px" width="160px" height="44px" borderRadius="16px" />
-        <Text>1번. 어쩌고 저쩌고</Text>
-        <Text>2번. 어쩌고 저쩌고</Text>
-        <Text>3번. 어쩌고 저쩌고</Text>
+        <FadeInSection>
+          <TextBold>웹 개발 기본 과정을 합께 배웁니다</TextBold>
+          <Text>멋쟁이 사자처럼 11기 아기사자들은 TECHIT 사이트에서 진행되는 개발 강의를 듣게 됩니다.</Text>
+          <Click onClick={console.log("open techit")}>
+            <Button text="TECHIT 둘러보기" fontSize="18px" width="160px" height="44px" borderRadius="16px" />
+          </Click>
+          <Text>1번. 어쩌고 저쩌고</Text>
+          <Text>2번. 어쩌고 저쩌고</Text>
+          <Text>3번. 어쩌고 저쩌고</Text>
+        </FadeInSection>
       </Section4>
       <Section5>
-        <LogoFooterContainer src={logoFooter} />
-        <Space height="25vh" />
+        <FadeInSection>
+          <LogoFooterContainer src={logoFooter} />
+          <Space height="25vh" />
+        </FadeInSection>
       </Section5>
     </>
   );
@@ -72,7 +109,16 @@ const Section1 = styled.div`
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
-  height: 100vh;
+  height: 120vh;
+  width: 100%;
+`;
+
+const CenterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 `;
 
 const ButtonContainer = styled.div`
@@ -102,13 +148,19 @@ const Section2 = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  align-items: flex-start;
-  height: 100vh;
+  align-items: flex-end;
+  height: 120vh;
+  width: 100%;
+`;
+
+const RowContainer = styled.div`
+  display: flex;
+  flex-direction: row;
 `;
 
 const TextContainer = styled.div`
-  position: absolute;
-  right: 20vh;
+  display: flex;
+  flex-direction: column;
 `;
 
 const TextBold = styled.div`
@@ -127,13 +179,14 @@ const Section3 = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  align-items: center;
-  height: 100vh;
+  align-items: flex-start;
+  height: 120vh;
+  width: 100%;
 `;
 
 const TimeTable = styled.div`
-  position: absolute;
-  left: 20vh;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Text = styled.div`
@@ -146,7 +199,8 @@ const Section4 = styled.div`
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
-  height: 100vh;
+  height: 120vh;
+  width: 100%;
 `;
 
 const Section5 = styled.div`
@@ -155,9 +209,27 @@ const Section5 = styled.div`
   justify-content: flex-end;
   align-items: center;
   height: 100vh;
+  width: 100%;
 `;
 
 const LogoFooterContainer = styled.img`
   width: 524px;
   height: 132px;
 `;
+
+const FadeContainer = styled.div`
+  opacity: 0;
+  transform: translateY(20vh);
+  visibility: hidden;
+  transition: opacity 1200ms ease-out, transform 600ms ease-out, visibility 1200ms ease-out;
+  will-change: opacity, transform, visibility;
+  ${(props) =>
+    props.isVisible &&
+    `
+      opacity: 1;
+      transform: none;
+      visibility: visible;
+    `}
+`;
+
+const Click = styled.div``;
