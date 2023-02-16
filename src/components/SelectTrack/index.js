3577f6690b5460b1ca97991974fdae7781d9f1a1
dianexjeong <zuphyr14@gmail.com>
2023-02-16 20:15:03 +0900

    chore: apply font family to selectcontainer

--- src/components/SelectTrack/index.js
+++ src/components/SelectTrack/index.js
@@ -1,40 +1,40 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import styled from "styled-components";
 
 export const SelectTrack = () => {
-    const selectList = ["백엔드", "프론트엔드"];
-    const [Selected, setSelected] = useState('지원하실 트랙을 선택해주세요');
-  
-    const handleSelect = (e) => {
-      setSelected(e.target.value);
-    };
+  const selectList = ["백엔드", "프론트엔드"];
+  const [selected, setSelected] = useState("지원하실 트랙을 선택해주세요");
 
-    const options = [
-        { value: '백엔드', label: '백엔드' },
-        { value: '프론트엔드', label: '프론트엔드' },
-      ];
-    
-    return (
-      <>
-        <SelectContainer onChange={handleSelect} value={Selected}>
-            {selectList.map((item) => (
-                <SelectOption value={item} key={item}>
-                    {item}
-                </SelectOption>
-            ))}
-        </SelectContainer>
-    </>
-    );
+  const handleSelect = (e) => {
+    setSelected(e.target.value);
   };
 
-  const SelectContainer = styled.select`
+  const options = [
+    { value: "백엔드", label: "백엔드" },
+    { value: "프론트엔드", label: "프론트엔드" },
+  ];
+
+  return (
+    <>
+      <SelectContainer onChange={handleSelect} value={selected}>
+        {selectList.map((item) => (
+          <SelectOption value={item} key={item}>
+            {item}
+          </SelectOption>
+        ))}
+      </SelectContainer>
+    </>
+  );
+};
+
+const SelectContainer = styled.select`
   width: 460px;
   height: 45px;
   border: 1px solid gray;
   border-radius: 10px;
   padding: 0px 15px 0px 15px;
-  font-family: D2Coding, sans-serif;
+  font-family: Pretendard;
   font-size: 20px;
   background-color: black;
   color: white;
@@ -43,8 +43,8 @@ export const SelectTrack = () => {
   &:focus {
     outline: none;
   }
-
 `;
 
-const SelectOption = styled.option` 
-`
+const SelectOption = styled.option`
+  font-family: Pretendard;
+`;
