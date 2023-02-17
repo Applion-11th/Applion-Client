69107de9d18adfd1830fdc0dbca41eff7b0d2c25
dianexjeong <zuphyr14@gmail.com>
2023-02-17 20:43:31 +0900

    chore: remove console.log

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -78,7 +78,6 @@ const Info = () => {
       )
       .then((response) => {
         if (response.status === 200) {
-          console.log("working");
           navigate("/apply");
         }
       })
