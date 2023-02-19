4246964e9c950a17585a9da4c7ae8465acfc231c
dianexjeong <zuphyr14@gmail.com>
2023-02-19 16:19:13 +0900

    feat: align text in /complete to center

--- src/pages/Complete.js
+++ src/pages/Complete.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import logoSogang from "../assets/logoSogang.svg";
 import { Space, Button } from "../components/atoms";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
 const Complete = () => {
   const [year, setYear] = useState();
@@ -19,35 +19,37 @@ const Complete = () => {
     navigate("/");
   };
 
-  var Year; var Month; var DayDate; var Time; var Minute;
+  var Year;
+  var Month;
+  var DayDate;
+  var Time;
+  var Minute;
   const ShowChangedate = () => {
-      if (localStorage.getItem("access_token")) {
-        axios
-          .get(`${process.env.REACT_APP_SERVER_APPLY_URL}/${localStorage.getItem("id")}/`, {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-            },
-          })
-          .then((response) => {
-            if (response.status === 200) {
-              console.log(response);
-              const Changedate = new Date(response.data.updated_at);
-              setYear(Changedate.getFullYear());
-              setMonth(Changedate.getMonth() + 1);
-              setDay(Changedate.getDate());
-              setTime(Changedate.getHours());
-              setMinute(Changedate.getMinutes());
-              console.log(Year);
-            }
-          })
-          .catch((error) => {
-            console.log(error.request.response);
-          });
-      }
-      return (
-        <TextMedium>{`최종 제출 시각: ${year}년 ${month}월 ${day}일 ${time}시 ${minute}분`}</TextMedium>
-      )
-    };
+    if (localStorage.getItem("access_token")) {
+      axios
+        .get(`${process.env.REACT_APP_SERVER_APPLY_URL}/${localStorage.getItem("id")}/`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+          },
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            console.log(response);
+            const Changedate = new Date(response.data.updated_at);
+            setYear(Changedate.getFullYear());
+            setMonth(Changedate.getMonth() + 1);
+            setDay(Changedate.getDate());
+            setTime(Changedate.getHours());
+            setMinute(Changedate.getMinutes());
+            console.log(Year);
+          }
+        })
+        .catch((error) => {
+          console.log(error.request.response);
+        });
+    }
+    return <TextMedium>{`최종 제출 시각: ${year}년 ${month}월 ${day}일 ${time}시 ${minute}분`}</TextMedium>;
+  };
 
   useEffect(() => {
     if (!localStorage.getItem("access_token")) {
@@ -58,10 +60,10 @@ const Complete = () => {
   return (
     <>
       <Flex>
-        <Space height="20px" />
+        <Space height="10px" />
         <Text>지원 완료되었습니다.</Text>
         <Space height="20px" />
-        <ShowChangedate/>
+        <ShowChangedate />
         <Space height="20px" />
         <ImgContainer src={logoSogang} />
         <Description>
@@ -70,12 +72,12 @@ const Complete = () => {
           </TextContainer>
           <Space height="10px" />
           <TextContainer>
-            {/* <Space margin="0px 36px 0px 0px" /> */}
+            <Space margin="0px 36px 0px 0px" />
             <TextMedium>면접 | 2023년 3월 15일(수) - 2023년 3월 17일(금)</TextMedium>
           </TextContainer>
           <Space height="10px" />
           <TextContainer>
-            {/* <Space margin="0px 80px 0px 0px" /> */}
+            <Space margin="0px 80px 0px 0px" />
             <TextSmall>서류 발표 후, 면접 날짜 선택</TextSmall>
           </TextContainer>
           <Space height="10px" />
@@ -83,7 +85,7 @@ const Complete = () => {
             <TextMedium>최종 발표 | 2023년 3월 20일(월)</TextMedium>
           </TextContainer>
         </Description>
-        <Space height="20px" />
+        <Space height="25px" />
         <ButtonContainer>
           <Click onClick={gotoMain}>
             <Button width="167px" height="47px" text="홈으로 돌아가기" fontSize="16px" borderRadius="10px" />
@@ -120,10 +122,10 @@ const Flex = styled.div`
 `;
 
 const Description = styled.div`
-  width: 40%;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
+
   font-weight: 400;
   font-size: 18px;
 `;
